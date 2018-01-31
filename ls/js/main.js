3bfef74880de4665acfcdcd757b7a88011a98652
caiocosta08 <31280039+caiocosta08@users.noreply.github.com>
2018-01-31 17:37:54 -0300

    Add files via upload

---
+++ ls/js/main.js
@@ -0,0 +1,35 @@
+//let url = "https://chef-em-casa.herokuapp.com/json/dados.json"
+let url = "http://pokeapi.co/api/v2/pokemon/1"
+
+$(document).ready(function(){
+
+    $.getJSON(url)
+    .done(function (response){
+            document.getElementById('teste').innerHTML = '<h4>' + response +  ' </h4>'
+    })
+})
+/*
+
+
+
+
+
+
+
+
+
+
+
+    $("btnCompare").click(function(){
+//        $(this).button('toggle');
+        document.getElementById('teste').innerHTML = "<h4>deu certo</h4>"
+        alert("adsad");
+    });
+
+    function searchGit(){
+        $.getJSON("https://api.github.com/repos/caiocosta08/chiara-luce")
+        .done(function (response){
+                document.getElementById('teste').innerHTML = "<h4>deu certo</h4>"
+        })
+    }
+*/
